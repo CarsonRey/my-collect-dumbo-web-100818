de05dab8492d975680510c83b786095a6a6cc7a1
Carson Rey <c.rey494@gmail.com>
2018-09-18 13:12:39 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,6 @@
 def my_collect(array) 
   storage = []
-  change_and_store = ->(element) {storage << element}
+  change_and_store = ->(language) {storage << language.upcase}
   i = 0 
   while i < array.length 
   yield change_and_store 
