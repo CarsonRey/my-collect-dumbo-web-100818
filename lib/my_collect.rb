a0df7aa7a3e881331d9be84f1ba2983fd105791c
Carson Rey <c.rey494@gmail.com>
2018-09-18 16:43:35 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 def my_collect(array) 
   storage = []
+  change_and_store = lambda {|element| storage << element}
   i = 0 
   while i < array.length 
   "#{yield}" 
   i += 1
 end
+storage
 end
 
