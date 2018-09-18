43acae1241757e35c993162bcff05de1dd73dfaa
Carson Rey <c.rey494@gmail.com>
2018-09-18 12:58:09 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,6 @@
 def my_collect(array) 
   storage = []
-  capitalize_and_store = ->(element) {storage << element.upcase}
+  capitalize_and_store = ->(element) {storage << element.capitalize}
   i = 0 
   while i < array.length 
   yield capitalize_and_store 
